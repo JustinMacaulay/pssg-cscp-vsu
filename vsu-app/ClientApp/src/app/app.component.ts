cdf2f912e315913e46b5826de1d9a7ba41bf894f
JustinMacaulay <justinkmacaulay@gmail.com>
2024-11-14 09:35:47 -0800

    VS-6971 Outage Banner (#93)
    * added outage banner that can be updated with secrets to display or not.
    
    * removing bad package lock
    
    * fixing a file I removed on accident

--- vsu-app/ClientApp/src/app/app.component.ts
+++ vsu-app/ClientApp/src/app/app.component.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ConfigService} from "./services/config.service";
+import {Configuration} from "./shared/interfaces/configuration.interface";
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '';
   previousUrl: string;
+  configuration: Configuration;
+  error = false;
   public isNewUser: boolean;
   public isDevMode: boolean;
 
-  constructor() {
+
+  constructor(
+    private configService: ConfigService,
+  ) { }
+
+  ngOnInit(): void {
+    this.configService.load()
+      .then((configuration) => {
+        console.log("Fetched Configuration:", configuration);
+        this.configuration = configuration;
+      })
+      .catch((error) => {
+        console.error("Failed to fetch configuration:", error);
+        this.error = error;
+      });
+  }
+
+  isOutage() {
+    if (!this.configuration || !this.configuration.outageEndDate || !this.configuration.outageStartDate || !this.configuration.outageMessage) {
+      return false;
+    }
+    const currentDate = moment().tz("America/Vancouver");
+    const outageStartDate = moment(this.configuration.outageStartDate).tz("America/Vancouver");
+    const outageEndDate = moment(this.configuration.outageEndDate).tz("America/Vancouver");
+    return currentDate.isBetween(outageStartDate, outageEndDate, null, '[]');
+  }
+
+  generateOutageDateMessage(): string {
+    const startDate = moment(this.configuration.outageStartDate).tz("America/Vancouver").format("MMMM Do YYYY, h:mm a");
+    const endDate = moment(this.configuration.outageEndDate).tz("America/Vancouver").format("MMMM Do YYYY, h:mm a");
+    return "The system will be down for maintenance from " + startDate + " to " + endDate;
   }
 
   isIE10orLower() {
